098dcf2d2da0e5dce764b68dbc446d45fcb13baf
Christian Moore <codeyellow97@gmail.com>
2018-12-08 16:45:21 -0700

    Completed the second module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +11,34 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var threshold = (int) Math.Ceiling(Students.Count * .20);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[threshold -1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
